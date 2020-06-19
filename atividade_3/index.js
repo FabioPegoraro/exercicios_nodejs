ff7cfba51cbf44e4f375ab9145c03850b55f4b58
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-19 20:46:31 -0300

    Resultado Exercicio 3

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,5 +1,19 @@
 // ## Importe a biblioteca ##
-
+let rs= require ("readline-sync")
 
 
 // ## Faça o código ##
+
+let nome = rs.question("Ola, qual o seu nome?\n")
+
+let base = rs.question(nome+", qual a medida da base do quadrado?\n")
+
+let altura = rs.question("Qual a medida da altura do quadrado?\n")
+
+let area = base*altura
+
+console.log("====================================")
+
+console.log(nome+", a area do quadrado é "+area)
+
+console.log("====================================")
