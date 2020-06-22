7547cab14382c8db939496a866ff66e8e366e611
Fabio Pegoraro <fabiopegoraro10@gmail.com>
2020-06-22 12:45:40 -0300

    Resultados Exerciocio 5

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,15 @@
 // ## Importe a biblioteca ##
 
-
+let rs = require("readline-sync")
 
 // ## Faça o código ##
+
+let numCaracteres = rs.question("Digite qualquer coisa\n")
+
+resultado = numCaracteres.length;
+
+console.log("====================================\n")
+
+console.log("Você digitou " + resultado+ " caracteres\n")
+
+console.log("====================================")
